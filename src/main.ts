2fa858c9b5af53c022f64d77aab339e9b61ce972
Tigran Ghalumyan <cashcarticash@icloud.com>
2025-03-04 00:21:25 +0400

    feat: add localstorage

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,17 @@ import "./style.css";
 import App from "./App.vue";
 
 const app = createApp(App);
-app.use(createPinia());
+const pinia = createPinia();
+
+app.use(pinia);
+pinia.use(({ store }) => {
+    const data = localStorage.getItem(store.$id);
+    if (data) {
+        store.$patch(JSON.parse(data));
+    }
+    store.$subscribe((mutation, state) => {
+        localStorage.setItem(store.$id, JSON.stringify(state));
+    });
+});
+
 app.mount("#app");
